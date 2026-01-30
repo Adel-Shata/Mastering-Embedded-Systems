04477c41acb6844759277e02638fff07421e2870
Adel Shata <adel.shata@gmail.com>
2026-01-30 06:39:45 -0800

    Added Assignments of unit 2 C basic Exp1

---
+++ Unit2_C_Programming/1_C_Basics/Assignments/EX1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Ex1.c
+ Author      : Adel Shata
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("C Programing"); /* prints !!!Hello World!!! */
+	return 0;
+}
